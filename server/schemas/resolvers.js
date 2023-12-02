0f1f5fb688f12289b3deef761e1c14dba50f0cf8
James Peltier <peltierjames@gmail.com>
2023-12-02 11:22:00 -0800

    updated server folders

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,77 @@
+const { User } = require('../models');
+// import sign token function from auth
+const { signToken, AuthenticationError } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    async me(parent, args, context) {
+      const foundUser = await User.findOne({
+      _id: context.user._id, 
+      });
+  
+      if (!foundUser) {
+        // return throwError('Cannot find a user with this id!');
+        throw AuthenticationError;
+      }
+  
+      return (foundUser);
+    }
+  
+  },
+  Mutation: {
+    async createUser(parent, args, context) {
+    const user = await User.create(args);
+
+    if (!user) {
+      throw AuthenticationError;
+    }
+    const token = signToken(user);
+    return ({ token, user });
+  },
+  // login a user, sign a token, and send it back (to client/src/components/LoginForm.js)
+  // {body} is destructured req.body
+  async login(parent, args, context) {
+    const user = await User.findOne({ $or: [{ username: args.username }, { email: args.email }] });
+    if (!user) {
+      throw AuthenticationError;
+    }
+
+    const correctPw = await user.isCorrectPassword(args.password);
+
+    if (!correctPw) {
+      throw AuthenticationError;
+    }
+    const token = signToken(user);
+    return ({ token, user });
+  },
+  // save a book to a user's `savedBooks` field by adding it to the set (to prevent duplicates)
+  // user comes from `req.user` created in the auth middleware function
+  async saveBook(parent, args, context) {
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $addToSet: { savedBooks: args } },
+        { new: true, runValidators: true }
+      );
+      return (updatedUser);
+    } catch (err) {
+      console.log(err);
+      throw AuthenticationError;
+    }
+  },
+  // remove a book from `savedBooks`
+  async deleteBook(parent, args, context) {
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: context.user._id },
+      { $pull: { savedBooks: { bookId: args.bookId } } },
+      { new: true }
+    );
+    if (!updatedUser) {
+      throw AuthenticationError;
+    }
+    return (updatedUser);
+  },
+  },
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const typeDefs = `
+ type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+  
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+   createUser(username: String!, email: String!, password: String!): Auth
+   login(email: String!, password: String!): Auth
+   saveBook(authors: [String], description: String, title: String, bookId: String, image: String, link: String): User
+   deleteBook(bookId: String): User
+  }
+`;
+
+module.exports = typeDefs;
