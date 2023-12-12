b40f6bfb4a331be36501c9b539058dbea2821333
James Peltier <peltierjames@gmail.com>
2023-12-11 18:09:00 -0800

    final code

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -7,45 +7,26 @@ import {
   Col
 } from 'react-bootstrap';
 
-import { getMe, deleteBook } from '../utils/API';
-import {useMutation} from '@apollo/client';
+import {useMutation, useQuery} from '@apollo/client';
+import { GET_ME } from '../utils/API';
 import {DELETE_BOOK} from '../utils/mutations';
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  const [deleteBook, { error }] = useMutation(DELETE_BOOK);
+  const [deleteBook, deleteBookResult] = useMutation(DELETE_BOOK);
+  const getMe = useQuery(GET_ME);
   const [userData, setUserData] = useState({});
 
   // use this to determine if `useEffect()` hook needs to run again
   const userDataLength = Object.keys(userData).length;
 
+
   useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-        console.log(response);
-        
-        const user = await response.json();
-        console.log(response);
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
+    if (getMe.data) {
+      setUserData(getMe.data.me);
+    }
+  }, [getMe.data]);
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
@@ -56,11 +37,8 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook({variables: {bookId}});
+      await deleteBook({variables: {bookId}});
 
-     
-      const updatedUser = response.data.deleteBook.user;
-      setUserData(updatedUser);
       // upon success, remove book's id from localStorage
       removeBookId(bookId);
     } catch (err) {
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,5 +1,6 @@
 import { ApolloClient, InMemoryCache, gql } from '@apollo/client';
 
+
 const client = new ApolloClient({
   uri: 'http://localhost:3001/graphql', 
   cache: new InMemoryCache()
